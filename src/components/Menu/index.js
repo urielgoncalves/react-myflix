84402f426f1642403c0bdc98bbd6c6315d5fdd5e
Uriel Rocha Gonçalves <uriel_goncalves@hotmail.com>
2020-07-31 23:39:38 +0100

    Creating Video and Categories pages; Created PageDefault and changed Menu components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 import Logo from '../../assets/images/logo.png';
 import './menu.css';
+import {Link} from 'react-router-dom';
 // import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            {/* <a href="/">
                 <img className="Logo" src={Logo} alt="MyFlix" />
-            </a>
+            </a> */}
+
+            <Link to="/">
+                <img clLinkssName="Logo" src={Logo} alt="MyFlix" />
+            </Link>
 
             {/* <ButtonLink className="ButtonLink" href="/" >
                 Novo vídeo
@@ -20,7 +25,10 @@ function Menu() {
                 Novo vídeo
             </Button> */}
 
-            <Button as="a" className="ButtonLink" href="/" >
+            {/* <Button as="a" className="ButtonLink" href="/cadastro/video" >
+                Novo vídeo
+            </Button> */}
+             <Button as={Link} className="ButtonLink" to="/cadastro/video" >
                 Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from './pages/Cadastro/Video'
+import CadastroCategoria from './pages/Cadastro/Categoria';
+
+const Pagina404 = () => (<div>Pagina 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+  // Adding router system around our app
+  <BrowserRouter>
+    <Switch>
+      {/* <Route component={()=>(<div>Pagina 404</div>)} /> */}
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route component={Pagina404} />
+    </Switch>
+    {/* <App /> */}
+  </BrowserRouter>,
   document.getElementById('root')
 );
