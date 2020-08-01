e848660dbf8455e7eac60f454f91458ed15a3e00
Uriel Rocha Gonçalves <uriel_goncalves@hotmail.com>
2020-08-01 08:13:46 +0100

    Added category page and Carousel on Home Page

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { VideoCardGroupContainer, VideoCardList, Title, ExtraLink } from './styles';
+import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
 import VideoCard from './components/VideoCard';
+import Slider, { SliderItem } from './components/Slider';
 
-function VideoCardGroup({
+function Carousel({
   ignoreFirstVideo,
   category,
 }) {
@@ -24,25 +25,74 @@ function VideoCardGroup({
           }
         </>
       )}
-      <VideoCardList>
+      <Slider>
         {videos.map((video, index) => {
           if (ignoreFirstVideo && index === 0) {
             return null;
           }
 
           return (
-            <li key={video.titulo}>
+            <SliderItem key={video.titulo}>
               <VideoCard
                 videoTitle={video.titulo}
                 videoURL={video.url}
                 categoryColor={categoryColor}
               />
-            </li>
+            </SliderItem>
           );
         })}
-      </VideoCardList>
+      </Slider>
     </VideoCardGroupContainer>
   );
 }
 
-export default VideoCardGroup;
+export default Carousel;
+
+// import React from 'react';
+// import { VideoCardGroupContainer, VideoCardList, Title, ExtraLink } from './styles';
+// import VideoCard from './components/VideoCard';
+
+// function VideoCardGroup({
+//   ignoreFirstVideo,
+//   category,
+// }) {
+//   const categoryTitle = category.titulo;
+//   const categoryColor = category.cor;
+//   const categoryExtraLink = category.link_extra;
+//   const videos = category.videos;
+//   return (
+//     <VideoCardGroupContainer>
+//       {categoryTitle && (
+//         <>
+//           <Title style={{ backgroundColor: categoryColor || 'red' }}>
+//             {categoryTitle}
+//           </Title>
+//           {categoryExtraLink && 
+//             <ExtraLink href={categoryExtraLink.url} target="_blank">
+//               {categoryExtraLink.text}  
+//             </ExtraLink>
+//           }
+//         </>
+//       )}
+//       <VideoCardList>
+//         {videos.map((video, index) => {
+//           if (ignoreFirstVideo && index === 0) {
+//             return null;
+//           }
+
+//           return (
+//             <li key={video.titulo}>
+//               <VideoCard
+//                 videoTitle={video.titulo}
+//                 videoURL={video.url}
+//                 categoryColor={categoryColor}
+//               />
+//             </li>
+//           );
+//         })}
+//       </VideoCardList>
+//     </VideoCardGroupContainer>
+//   );
+// }
+
+// export default VideoCardGroup;
--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -1,17 +1,49 @@
-import React from 'react'; 
-import PageDefault from '../../../components/PageDefault'
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 
-function CadastroCategoria(){
-    return(
-        <PageDefault>
-            <h1>Cadastro de categoria</h1>
+function CadastroCategoria() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
 
-            <Link to="/">
-                Ir para Home
-            </Link>
-        </PageDefault>
-    )
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroCategoria;
+// import React from 'react'; 
+// import PageDefault from '../../../components/PageDefault'
+// import {Link} from 'react-router-dom';
+
+// function CadastroCategoria(){
+//     return(
+//         <PageDefault>
+//             <h1>Cadastro de categoria</h1>
+
+//             <Link to="/">
+//                 Ir para Home
+//             </Link>
+//         </PageDefault>
+//     )
+// }
+
+// export default CadastroCategoria;
