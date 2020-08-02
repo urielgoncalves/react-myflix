9e27b1822e808f5a06a0d63a5dfd416f0056350d
Uriel Rocha Gonçalves <uriel_goncalves@hotmail.com>
2020-08-02 16:20:05 +0100

    eslint fixes

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function FormField({label, type, name, value, onChange}){
-    return(
-        <div>
-        <label>
-          {label}:
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  const fieldId = `id_${name}`;
+  return (
+    <div>
+      <label htmlFor={fieldId}>
+        {label}
+        :
         <input
-            name={name}
-            type={type}
-            value={value}
-            onChange={onChange}
-          />
-        </label>
-      </div>
-    )
+          id={fieldId}
+          name={name}
+          type={type}
+          value={value}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
 }
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
 
 export default FormField;
--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
 import Menu from '../Menu';
 import Footer from '../Footer';
-import styled from 'styled-components';
 
 const Main = styled.main`
   background-color: var(--black);
@@ -12,8 +12,8 @@ const Main = styled.main`
   padding-right: 5%;
 `;
 
-function PageDefault({children}){
-    return (
+function PageDefault({ children }) {
+  return (
         <>
             <Menu />
                 <Main>
@@ -21,7 +21,7 @@ function PageDefault({children}){
                 </Main>
             <Footer />
         </>
-    )
+  )
 }
 
 export default PageDefault;
